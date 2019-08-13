7bb4e30e8051a5f5650c261089ead7565b85cb67
Marta <martaglowacka2014@gmail.com>
2019-08-13 13:43:38 +0200

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,26 +8,27 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected correctly to server");
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        //find all the dishes
+        //exec return promise and ensures evth is ok
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        //empty JS object - removes evth
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
