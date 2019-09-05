d7caec595b3199217bfab4305360c8822f8c0bd1
Marta <martaglowacka2014@gmail.com>
2019-09-05 11:30:52 +0200

    Final Assignment 4

--- conFusionServer/routes/favouritesRouter.js
+++ conFusionServer/routes/favouritesRouter.js
@@ -14,8 +14,7 @@ favouriteRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 //all dishes of exact user
 .get(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
-   // if(req.user._id.equals(dish.comments.id(req.params.commentId).author)) { //if user that performs the action is equal to user that submitted comment, allow operation
-  
+   
    Favourites.findOne({user: req.user._id})
    .populate('user')
    .then((favourite) => { //favourite doc returned correctly
@@ -45,23 +44,21 @@ favouriteRouter.route('/')
     .then((favourite) => { //favourite doc returned correctly
       if(favourite) {
 
+         //Async function, to all async operations in for loop execute earlier before save
          async function run() {
             console.log('Start for');
 
+            //Comparising dishes in body
             for(var i  = (req.body.length - 1); i >= 0; i--) 
             {
                console.log('In for loop');
-               //for (var i = (favourite.dishes.length - 1); i >= 0; i--)
-               //if the dish from the body (search by id) doesn't exist on the list - if list is not empty
-               // if(favourite.dishes.length === 0) {
-               //    console.log('List empty, detected\n');
-               //    favourite.dishes.push(favourite.dishes.id(req.body[i]._id));
-               // }
-               //else {
+
+               //With dishes on favourite list
                   if(favourite.dishes.findIndex(x => x._id == req.body[i]._id) === -1)
                   {
                      console.log('Dish is not on the list'); 
-      
+
+                     //If dish is not on the favourite list, add it
                      await Dishes.findById(req.body[i]._id)
                      .then((dish) => {         
                         if(dish) {                           
@@ -79,15 +76,15 @@ favouriteRouter.route('/')
                      //alternative of dish?   favourite.dishes.push(favourite.dishes.id(favourite.dishes[i]._id));       
       
                   }
+                  // If dish is already on the list, do nothing
                   else{
                      console.log('Dish ' + req.body[i]._id + ' already exists');
                   }
-               //}
-
+            
               
             }//end for 
-            console.log('end for');
 
+            //Save dishes added to favourite list and populate user
             console.log('Start save');
             await favourite.save()
             .then((favourite) => {
@@ -105,15 +102,21 @@ favouriteRouter.route('/')
          }
          run().catch(err => console.error(err.stack));        
       }
+      //If favourite list does not exist
       else {
+
          async function run() {
             console.log('1 Favourite doc of this user not found');
+
+            //Two arrays - first add all dishes from the body, even with duplicates
             var myFirstDishesOnList = []; 
+            //Create array from array above, but filter duplicates
             var uniqueArray = [];
 
             for(var i  = (req.body.length - 1); i >= 0; i--) 
             {
                console.log('id: ' + (req.body[i]._id));
+
                await Dishes.findById(req.body[i]._id)
                .then((dish) => {
 
@@ -136,6 +139,7 @@ favouriteRouter.route('/')
             }
 
 
+            //Create new favourite list from unique Array of dishes to be added
             await Favourites.create({"user": req.user._id, "dishes": uniqueArray})
             .then((favourite) => {
                console.log("Save");
@@ -155,8 +159,14 @@ favouriteRouter.route('/')
       }
    });
 })
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+   res.statusCode = 403;
+   res.end('PUT operation not supported on /favorites');
+})
 //favourite doc completely deleted
 .delete(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
+
+   //Remove the whole favourite list of specific user
    Favourites.findOneAndRemove({user: req.user._id})
     .then((resp) => { //favourite doc returned correctly
      
@@ -171,18 +181,26 @@ favouriteRouter.route('/')
 
 
 
+/**
+ * Route on favourite/dishId 
+ * operation on specific dish on favourite list
+ */
 
 favouriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 //chceck, if dish is already on the list - indexOf method
 //particular dish added
+.get(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
+   res.statusCode = 403;
+   res.end('GET operation not supported on /favorites/'+ req.params.dishId);
+})
 .post(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
-   //if favourite doc doesn't exist, create one and then add
+ 
     Favourites.findOne({user: req.user._id})
     .then((favourite) => { //favourite doc returned correctly
       if(favourite) { //if favourite document of user found successfully
          
-         //if dish is on the list
+         //if dish is on the list - check if index is not -1
          console.log(favourite.dishes.findIndex(x => x._id === req.params.dishId));
 
          //It compares _id field in dishes with dishId - type is different, so use == instead of ===
@@ -201,15 +219,16 @@ favouriteRouter.route('/:dishId')
                 favourite.dishes.push(dish);
                 favourite.user = req.user._id;
 
+                //Save added dish with favourite list
                 favourite.save()
                 .then((favourite) => {
                    console.log(favourite.dish + " saved to fav list.");
                    favourite
                    .populate('user') //I dont't understand, why we don't populate dishes, but this way it works               
                    
-                     res.statusCode = 201;
-                     res.setHeader('Content-Type', 'application/json');
-                     res.json(favourite);
+                   res.statusCode = 201;
+                   res.setHeader('Content-Type', 'application/json');
+                   res.json(favourite);
                   
                 })
 
@@ -228,6 +247,8 @@ favouriteRouter.route('/:dishId')
                 //if dish is found       
                 if(dish != null) {  
                   console.log('2 Such dish exists ', dish);
+
+                  //Create new favourite list with found dish
                   Favourites.create({"user": req.user._id, "dishes": [dish]})
                   .then((favourite) => {
                      favourite.save()
@@ -257,29 +278,18 @@ favouriteRouter.route('/:dishId')
     .catch((err) => next(err)); //favourite doc not found (not possible, because the search is by user id)
     
 })
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+   res.statusCode = 403;
+   res.end('PUT operation not supported on /favorites/'+ req.params.dishId);
+})
 //specific dish deleted
 .delete(cors.corsWithOptions, authenticate.verifyUser, (req,res,next) => {
 
-   
-
+   //Find list of specific user
    Favourites.findOne({user: req.user._id})
    .then((favourite) => {
-      //Dishes.findByIdAndRemove(req.params.dishId)
-      
-      // Dishes.findById(req.params.dishId)
-      // .then((dish) => {
-      //    if (favourite.dishes.indexOf( dish) !== -1)
-      //    {
-      //       console.log('It is on the official dish list');
-      //    }
-      //    else
-      //    {
-      //       console.log('Dish does not longer exist on official dish list! I am glad you are deleting it');
-      //    }
-      // },  (err) => next(err));
-      
-      
-
+   
+      //Remove specific dish from favourite list - remember it may end up empty - it is not a problem for us
       favourite.dishes.id(req.params.dishId).remove();
       favourite.save()
       .then((resp) => {
@@ -288,19 +298,6 @@ favouriteRouter.route('/:dishId')
             res.json(resp); 
       });
 
-
-      //if fav list is empty, delete fav document
-      // if(favourite.dishes.length === 0) {
-      //    Favourites.findOneAndRemove({user: req.user._id})
-      //    .then((resp) => { //favourite doc returned correctly
-         
-      //          res.statusCode = 200;   
-      //          res.setHeader('Content-Type', 'application/json');
-      //          res.json(resp); //this will be handled by app
-            
-      //    }, (err) => next(err))
-      //    .catch((err) => next(err));
-      // }
 
    }, (err) => next(err))
    .catch((err) => next(err));
